122189e349afcdfccac62622780115d6a99bed47
Qianhe Xiao <lapis_xiao@outlook.com>
2025-05-05 14:46:19 +0800

    hw4 completed.

--- src/components/Classroom.jsx
+++ src/components/Classroom.jsx
@@ -1,25 +1,89 @@
-import { Button, Container, Form, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Button, Container, Form, Row, Col } from "react-bootstrap";
+import Student from "./Student";
+import { Pagination } from "react-bootstrap";
 
 const Classroom = () => {
+    const [students, setStudents] = useState([]);
+    const [searchName, setSearchName] = useState("");
+    const [searchMajor, setSearchMajor] = useState("");
+    const [searchInterest, setSearchInterest] = useState("");
+    const [active, setActive] = useState(1);
+
+    useEffect(() => {
+        fetch("https://cs571.org/rest/s25/hw4/students", {
+            headers: {
+                "X-CS571-ID": "bid_0dc79fe52f6a30a5bfdd920f25f4b01da3803f43cce3d87dfb17406e23676d1a"
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => {
+           setStudents(data);
+           console.log(data);
+        }) 
+    }, [])
+
+    let filterStudents = students.filter((student) => {
+        let flag = true;
+        if (searchName && !(student.name.first + " " + student.name.last).toLowerCase().includes(searchName.toLowerCase())){
+            flag = false;
+        }
+        if (searchMajor && !student.major.toLowerCase().includes(searchMajor.toLowerCase())){
+            flag = false;
+        }
+        if (searchInterest && !student.interests.some((interest) => interest.toLowerCase().includes(searchInterest.toLowerCase()))){
+            flag = false;
+        }
+        return flag;
+    })
+
+    let pageNum = Math.ceil(filterStudents.length / 24); 
+    let pageItems = [];
+    pageItems.push(
+        <Pagination.Prev key={0} onClick={() => setActive(active - 1)} disabled={active === 1}>Previous</Pagination.Prev>
+    )
+    for(let i = 1; i <= pageNum; i++){
+        pageItems.push(
+            <Pagination.Item key={i} active={active === i} onClick={() => setActive(i)}>{i}</Pagination.Item>
+        )
+    }
+    pageItems.push(
+        <Pagination.Next key={pageNum + 1} onClick={() => setActive(active + 1)} disabled={active === pageNum}>Next</Pagination.Next>
+    )
+
+    let sliceStudents = filterStudents.slice((active - 1) * 24, active * 24);
+
+
     return <div>
         <h1>Badger Book</h1>
         <p>Search for students below!</p>
         <hr />
         <Form>
             <Form.Label htmlFor="searchName">Name</Form.Label>
-            <Form.Control id="searchName"/>
+            <Form.Control id="searchName" value={searchName} onChange={(e) => setSearchName(e.target.value)}/>
             <Form.Label htmlFor="searchMajor">Major</Form.Label>
-            <Form.Control id="searchMajor"/>
+            <Form.Control id="searchMajor" value={searchMajor} onChange={(e) => setSearchMajor(e.target.value)}/>
             <Form.Label htmlFor="searchInterest">Interest</Form.Label>
-            <Form.Control id="searchInterest"/>
+            <Form.Control id="searchInterest" value={searchInterest} onChange={(e) => setSearchInterest(e.target.value)}/>
             <br />
-            <Button variant="neutral">Reset Search</Button>
+            <Button variant="neutral" onClick={() => {
+                setSearchInterest("");
+                setSearchMajor("");
+                setSearchName("");
+            }}>Reset Search</Button>
         </Form>
+        <p>There are {filterStudents.length} student(s) matching your search.</p>
         <Container fluid>
             <Row>
                 { /* TODO Students go here! */ }
+                {
+                    sliceStudents.map(student => 
+                        <Col key={student.id} xs={12} sm={12} md={6} lg={4} xl={3}><Student {...student}></Student></Col>
+                    )
+                }
             </Row>
-        </Container>
+        </Container> 
+        <Pagination>{pageItems}</Pagination>
     </div>
 
 }
--- src/components/Student.jsx
+++ src/components/Student.jsx
@@ -2,6 +2,14 @@ const Student = (props) => {
     return <div>
         <h2>{props.name.first} {props.name.last}</h2>
         {/* TODO Student data goes here! */}
+        <h3>{props.major}</h3>
+        <p>{props.name.first} is taking {props.numCredits} and is {props.fromWisconsin ? '': 'NOT ' }from Wisconsin.</p>
+        <p>They have {props.interests.length} interests including...</p>
+        <ul>
+            {props.interests.map((interest, index) => {
+                return <li key={index}>{interest}</li>
+            })}
+        </ul>
     </div>
 }
 
